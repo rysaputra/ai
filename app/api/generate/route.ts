acc1fe1a121239260fe69344b0aba14b7ba181ef
Ryo <158389572+rysaputra@users.noreply.github.com>
2024-08-19 13:23:54 +0700

    first commit

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -8,7 +8,7 @@ const apiKey = process.env.GEMINI_API_KEY as string;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: 'gemini-1.5-flash',
+  model: 'gemini-1.5-pro',
 });
 
 const generationConfig = {
